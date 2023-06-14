477d89db113af5d7d7170aa839acd01d6455bc55
Changooo <cgl1234321@gmail.com>
2023-06-14 20:37:24 +0900

    first commit

---
+++ app/src/main/java/skku/team/g4u/SellerPageActivity.kt
@@ -0,0 +1,11 @@
+package skku.team.g4u
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SellerPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seller_page)
+    }
+}
---
+++ app/src/main/java/skku/team/g4u/SellerRegisterActivity.kt
@@ -0,0 +1,11 @@
+package skku.team.g4u
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SellerRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seller_register)
+    }
+}
