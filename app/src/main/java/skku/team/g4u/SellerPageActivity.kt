05561d0a42c0e3ffc8c8482f5a00673dbfdc145e
wonyounglee <lwy970327@naver.com>
2023-06-14 23:24:03 +0900

    제대로 되는지 확인

--- app/src/main/java/skku/team/g4u/SellerPageActivity.kt
+++ app/src/main/java/skku/team/g4u/SellerPageActivity.kt
@@ -2,10 +2,12 @@ package skku.team.g4u
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class SellerPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_seller_page)
+        Log.d("dd","제대로되나?")
     }
 }
